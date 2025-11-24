1ba9fdf4e53b22be2859af263bf7e5698ab35126
2482093 <2482093410@qq.com>
2025-11-24 14:55:12 +0800

    project init

---
+++ src/main/java/top/lyh/SpringBootLiveApplication.java
@@ -0,0 +1,13 @@
+package top.lyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLiveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLiveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/lyh/SpringBootLiveApplicationTests.java
@@ -0,0 +1,13 @@
+package top.lyh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLiveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
